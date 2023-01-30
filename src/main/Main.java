874a0c071a4e28c0b2851727e7463e2845fd457b
rudineybarbosa <rudiney1h@gmail.com>
2023-01-30 15:44:50 -0300

    first commit

---
+++ src/main/Main.java
@@ -0,0 +1,19 @@
+package main;
+import java.util.Date;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		  Date start = new Date(); 
+		  PrimeNumbers primeNumbers = new PrimeNumbers();
+		  Integer max = Integer.parseInt(args[0]); 
+		  primeNumbers.generateNumbers(max);
+		  Date end = new Date(); 
+		  
+		  System.out.println(
+				  "Time " + (end.getTime() -
+				  start.getTime() + " ms")); double d = 10000000;
+		 
+	}
+}
